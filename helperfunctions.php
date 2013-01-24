68ca9eb9725e9e9b974d1e493ffdae82ccf505d9
nischayn22 <nischayn22@gmail.com>
2013-01-24 08:29:00 +0530

    formatting

--- helperfunctions.php
+++ helperfunctions.php
@@ -5,68 +5,68 @@
  */
 
 function httpRequest($url, $post="") {
-        global $settings;
+	global $settings;
 
-        $ch = curl_init();
-        //Change the user agent below suitably
-        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.9) Gecko/20071025 Firefox/2.0.0.9');
-        curl_setopt($ch, CURLOPT_URL, ($url));
-        curl_setopt( $ch, CURLOPT_ENCODING, "UTF-8" );
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt ($ch, CURLOPT_COOKIEFILE, $settings['cookiefile']);
-        curl_setopt ($ch, CURLOPT_COOKIEJAR, $settings['cookiefile']);
-        if (!empty($post)) curl_setopt($ch,CURLOPT_POSTFIELDS,$post);
-        //UNCOMMENT TO DEBUG TO output.tmp
-        //curl_setopt($ch, CURLOPT_VERBOSE, true); // Display communication with server
-        //$fp = fopen("output.tmp", "w");
-        //curl_setopt($ch, CURLOPT_STDERR, $fp); // Display communication with server
-        
-        $xml = curl_exec($ch);
-        
-        if (!$xml) {
-                throw new Exception("Error getting data from server ($url): " . curl_error($ch));
-        }
+	$ch = curl_init();
+	//Change the user agent below suitably
+	curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.9) Gecko/20071025 Firefox/2.0.0.9');
+	curl_setopt($ch, CURLOPT_URL, ($url));
+	curl_setopt( $ch, CURLOPT_ENCODING, "UTF-8" );
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt ($ch, CURLOPT_COOKIEFILE, $settings['cookiefile']);
+	curl_setopt ($ch, CURLOPT_COOKIEJAR, $settings['cookiefile']);
+	if (!empty($post)) curl_setopt($ch,CURLOPT_POSTFIELDS,$post);
+	//UNCOMMENT TO DEBUG TO output.tmp
+	//curl_setopt($ch, CURLOPT_VERBOSE, true); // Display communication with server
+	//$fp = fopen("output.tmp", "w");
+	//curl_setopt($ch, CURLOPT_STDERR, $fp); // Display communication with server
+	
+	$xml = curl_exec($ch);
+	
+	if (!$xml) {
+			throw new Exception("Error getting data from server ($url): " . curl_error($ch));
+	}
 
-        curl_close($ch);
-        
-        return $xml;
+	curl_close($ch);
+	
+	return $xml;
 }
 
 
 function login ( $site, $user, $pass, $token='') {
 
-        $url = $site . "/api.php?action=login&format=xml";
+	$url = $site . "/api.php?action=login&format=xml";
 
-        $params = "action=login&lgname=$user&lgpassword=$pass";
-        if (!empty($token)) {
-                $params .= "&lgtoken=$token";
-        }
+	$params = "action=login&lgname=$user&lgpassword=$pass";
+	if (!empty($token)) {
+			$params .= "&lgtoken=$token";
+	}
 
-        $data = httpRequest($url, $params);
-        
-        if (empty($data)) {
-                throw new Exception("No data received from server. Check that API is enabled.");
-        }
+	$data = httpRequest($url, $params);
 
-        $xml = simplexml_load_string($data);
-        if (!empty($token)) {
-                //Check for successful login
-                $expr = "/api/login[@result='Success']";
-                $result = $xml->xpath($expr);
+	if (empty($data)) {
+			throw new Exception("No data received from server. Check that API is enabled.");
+	}
 
-                if(!count($result)) {
-                        throw new Exception("Login failed");
-                }
-        } else {
-                $expr = "/api/login[@token]";
-                $result = $xml->xpath($expr);
+	$xml = simplexml_load_string($data);
+	if (!empty($token)) {
+			//Check for successful login
+			$expr = "/api/login[@result='Success']";
+			$result = $xml->xpath($expr);
 
-                if(!count($result)) {
-                        throw new Exception("Login token not found in XML");
-                }
-        }
-        
-        return $result[0]->attributes()->token;
+			if(!count($result)) {
+					throw new Exception("Login failed");
+			}
+	} else {
+			$expr = "/api/login[@token]";
+			$result = $xml->xpath($expr);
+
+			if(!count($result)) {
+					throw new Exception("Login token not found in XML");
+			}
+	}
+
+	return $result[0]->attributes()->token;
 }
 
 function deletepage( $pageName ) {
@@ -81,13 +81,13 @@ function deletepage( $pageName ) {
 		if( isset( $result[0] ) ) { // should skip Main Page
 			$deleteToken = urlencode((string)($result[0]['deletetoken']));
 			//now delete stuff
-			echo "deleting page $pageName <br/>";
+			echo "deleting page $pageName \n";
 			$url = $settings['publicwiki'] . "/api.php?action=delete&format=xml";
 			$params = "action=delete&title=$pageName&token=$deleteToken";
 			$data = httpRequest($url, $params);
 			$xml = simplexml_load_string($data);
 		}else{
-			echo "Unable to delete $pageName<br/>";
+			echo "Unable to delete $pageName\n";
 			//TODO show reasons here
 		}
 }
@@ -95,9 +95,9 @@ function deletepage( $pageName ) {
 function copypage( $pageName, $editToken ) {
 	global $settings;
 
-// Get Namespace
+	// Get Namespace
 	$parts = explode( ':', $pageName );
-	echo "Copying over $pageName<br/>";
+	echo "Copying over $pageName\n";
 	$url = $settings['privatewiki'] . "/api.php?format=xml&action=query&titles=$pageName&prop=revisions&rvprop=content";
 	$data = httpRequest($url, $params = '');
 	$xml = simplexml_load_string($data);
@@ -121,9 +121,9 @@ function copypage( $pageName, $editToken ) {
 	$url = $settings['publicwiki'] . "/api.php?format=xml&action=edit&title=$pageName&text=$content";
 	$data = httpRequest($url, $params = "format=xml&action=edit&title=$pageName&text=$content&token=$editToken");
 	$xml = simplexml_load_string($data);
-	//get status to display
+	// TODO: get status to display
 
-// Now copy category members too
+	// Now copy category members too
 	if( count( $parts ) === 2 && $parts[0] === 'Category') {
 		$url = $settings['privatewiki'] . "/api.php?format=xml&action=query&cmtitle=$pageName&list=categorymembers&cmlimit=10000";
 		$data = httpRequest($url, $params = '');
--- wikiImporter.php
+++ wikiImporter.php
@@ -12,63 +12,63 @@ include( 'settings.php' );
 $settings['cookiefile'] = "cookies.tmp";
 include( 'helperfunctions.php' );
 
-echo "Logging In to Public Wiki<br/>";
+echo "Logging In to Public Wiki\n";
 
-  try {
-                global $settings;
-				
-                $token = login($settings['publicwiki'],$settings['user'], $settings['pass']);
-                login($settings['publicwiki'],$settings['user'], $settings['pass'], $token);
-                echo ("SUCCESS<br>");
-        } catch (Exception $e) {
-                die("FAILED: " . $e->getMessage() . "<br>");
-        }
-		
-echo "Starting to delete pages one by one in public wiki, all images will be also deleted. <br/>";
+try {
+	global $settings;
+	$token = login($settings['publicwiki'],$settings['user'], $settings['pass']);
+	login($settings['publicwiki'],$settings['user'], $settings['pass'], $token);
+	echo ("SUCCESS\n");
+} catch (Exception $e) {
+	die("FAILED: " . $e->getMessage() . "\n");
+}
 
-        $url = $settings['publicwiki'] . "/api.php?action=query&list=allpages&format=xml&aplimit=10000"; // Hope this limit is enough large that we don't have the trouble to do this again and again using 'continue'
-        $data = httpRequest($url, $params = '');
-        $xml = simplexml_load_string($data);
-		//Check for successful login
-		$expr = "/api/query/allpages/p";
-		$result = $xml->xpath($expr);
-		foreach( $result as $page ) {
-			deletepage( (string)$page['title'] );
-		}
+echo "Starting to delete pages one by one in public wiki, all images will be also deleted. \n";
+
+$url = $settings['publicwiki'] . "/api.php?action=query&list=allpages&format=xml&aplimit=10000"; // Hope this limit is enough large that we don't have the trouble to do this again and again using 'continue'
+
+$data = httpRequest($url, $params = '');
+$xml = simplexml_load_string($data);
+//Check for successful login
+$expr = "/api/query/allpages/p";
+$result = $xml->xpath($expr);
+foreach( $result as $page ) {
+	deletepage( (string)$page['title'] );
+}
 // Bad design just copying code because File: are not returned above
-        $url = $settings['publicwiki'] . "/api.php?action=query&list=allpages&format=xml&apnamespace=6&aplimit=10000"; // Hope this limit is enough large that we don't have the trouble to do this again and again using 'continue'
-        $data = httpRequest($url, $params = '');
-        $xml = simplexml_load_string($data);
-		//Check for successful login
-		$expr = "/api/query/allpages/p";
-		$result = $xml->xpath($expr);
-		foreach( $result as $page ) {
-			deletepage( (string)$page['title'] );
-		}
+$url = $settings['publicwiki'] . "/api.php?action=query&list=allpages&format=xml&apnamespace=6&aplimit=10000"; // Hope this limit is enough large that we don't have the trouble to do this again and again using 'continue'
+$data = httpRequest($url, $params = '');
+$xml = simplexml_load_string($data);
+//Check for successful login
+$expr = "/api/query/allpages/p";
+$result = $xml->xpath($expr);
+foreach( $result as $page ) {
+	deletepage( (string)$page['title'] );
+}
 //all deletion done now :)
 
 //copy pages
 //now login to other wiki
-echo "Logging into private wiki now<br>";
-  try {
-                global $settings;
-                $token = login($settings['privatewiki'],$settings['user'], $settings['pass']);
-                login($settings['privatewiki'],$settings['user'], $settings['pass'], $token);
-                echo ("SUCCESS<br>");
-        } catch (Exception $e) {
-                die("FAILED: " . $e->getMessage() . "<br>");
-        }
-//die();
-echo "Now starting to import pages, categories and file-pages from the given file<br>";
+echo "Logging into private wiki now\n";
+try {
+	global $settings;
+	$token = login($settings['privatewiki'],$settings['user'], $settings['pass']);
+	login($settings['privatewiki'],$settings['user'], $settings['pass'], $token);
+	echo ("SUCCESS<br>");
+} catch (Exception $e) {
+	die("FAILED: " . $e->getMessage() . "<br>");
+}
+
+echo "Now starting to import pages, categories and file-pages from the given file\n";
 
 //get pagenames from file
 $pages = file($settings['file'], FILE_IGNORE_NEW_LINES);
 
-	//get token first
-	$url = $settings['publicwiki'] . "/api.php?format=xml&action=query&titles=Main_Page&prop=info|revisions&intoken=edit";
-	$data = httpRequest($url, $params = '');
-	$xml = simplexml_load_string($data);
-	$editToken = urlencode( (string)$xml->query->pages->page['edittoken'] );
+//get token first
+$url = $settings['publicwiki'] . "/api.php?format=xml&action=query&titles=Main_Page&prop=info|revisions&intoken=edit";
+$data = httpRequest($url, $params = '');
+$xml = simplexml_load_string($data);
+$editToken = urlencode( (string)$xml->query->pages->page['edittoken'] );
 foreach($pages as $pageName) {
 	copypage( $pageName, $editToken );
 }
