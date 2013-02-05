8b8a1aa62f2d434c1222c7156632a141a299d3f2
nischayn22 <nischayn22@gmail.com>
2013-02-05 10:27:22 +0530

    c/p fix

--- settings.php
+++ settings.php
@@ -11,9 +11,10 @@ $settings['publicWikiPassword'] = "password"; // Password
 // Settings for private wiki
 
 $settings['privateWiki'] = "http://myPrivate.com/w"; // Location of Api.php
-$settings['privateWikiUser'] = "Nischay Nahata"; // Username of account with read,write permissions
+$settings['privateWikiUser'] = "Nischay Nahata"; // Username of account with read permissions
 $settings['privateWikiPassword'] = "password"; // Password
 
 
 // File containing list of pages to be copied over, separated by newlines
+
 $settings['file'] = "clientPages.txt";
