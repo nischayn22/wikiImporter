97ed66a67550a540b152cdc6ea222ad8f8b93f31
nischayn22 <nischayn22@gmail.com>
2013-05-22 22:17:03 +0530

    fixed issue with importing pages with large content

--- helperfunctions.php
+++ helperfunctions.php
@@ -139,7 +139,7 @@ function copypage( $pageName, $editToken, $recursivelyCalled = true ) {
 
 	// now copy normal page
 	$content = urlencode( $content );
-	$url = $settings['publicWiki'] . "/api.php?format=xml&action=edit&title=$pageName&text=$content";
+	$url = $settings['publicWiki'] . "/api.php?format=xml&action=edit&title=$pageName";
 	$data = httpRequest($url, $params = "format=xml&action=edit&title=$pageName&text=$content&token=$editToken");
 	if ( $data == null ) {
 		// write to file that copy failed
