c84a41721ccd4c8778a6bcc90e22e3df1fa708c6
nischayn22 <nischayn22@gmail.com>
2014-04-04 09:36:20 +0530

    Extracted out MediaWiki_Api as a library.
    Some work still left to do. Also the library is currently bundled in while it
    is supposed to be maintained separately

--- helperfunctions.php
+++ helperfunctions.php
@@ -4,95 +4,9 @@
  * @license GPL v2 or later
  */
 
-function httpRequest($url, $post="", $retry = false, $retryNumber = 0) {
-	global $settings;
-
-	try {
-		$ch = curl_init();
-		//Change the user agent below suitably
-		curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.9) Gecko/20071025 Firefox/2.0.0.9');
-		if( $settings['serverAuth'] ) {
-			curl_setopt($ch, CURLOPT_USERPWD, $settings['AuthUsername'] . ":" . $settings['AuthPassword']);
-		}
-		curl_setopt($ch, CURLOPT_URL, ($url));
-		curl_setopt( $ch, CURLOPT_ENCODING, "UTF-8" );
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_COOKIEFILE, $settings['cookiefile']);
-		curl_setopt($ch, CURLOPT_COOKIEJAR, $settings['cookiefile']);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		if (!empty($post)) curl_setopt($ch,CURLOPT_POSTFIELDS,$post);
-		//UNCOMMENT TO DEBUG TO output.tmp
-		//curl_setopt($ch, CURLOPT_VERBOSE, true); // Display communication with server
-		//$fp = fopen("output.tmp", "w");
-		//curl_setopt($ch, CURLOPT_STDERR, $fp); // Display communication with server
-
-		$xml = curl_exec($ch);
-
-		if (!$xml) {
-			throw new Exception("Error getting data from server: " . curl_error($ch));
-		}
-
-		curl_close($ch);
-	} catch( Exception $e ) {
-		echo 'Caught exception: ',  $e->getMessage(), "\n";
-		if( !$retry && $retryNumber <3 ) {
-			echo "Retrying \n";
-			httpRequest($url, $post, true, $retryNumber++ );
-		} else {
-			echo "Could not perform action after 3 attempts. Skipping now...\n";
-			return null;
-		}
-	}
-	return $xml;
-}
-
-
-function login ( $site, $user, $pass, $token='') {
-
-	$url = $site . "/api.php?action=login&format=xml";
-
-	$params = "action=login&lgname=$user&lgpassword=$pass";
-	if (!empty($token)) {
-		$params .= "&lgtoken=$token";
-	}
-
-	$data = httpRequest($url, $params);
-
-	if (empty($data)) {
-		throw new Exception("No data received from server. Check that API is enabled.");
-	}
-
-	$xml = simplexml_load_string($data);
-	if (!empty($token)) {
-		//Check for successful login
-		$expr = "/api/login[@result='Success']";
-		$result = $xml->xpath($expr);
-
-		if(!count($result)) {
-			throw new Exception("Login failed");
-		}
-	} else {
-		$expr = "/api/login[@token]";
-		$result = $xml->xpath($expr);
-
-		if(!count($result)) {
-			throw new Exception("Login token not found in XML");
-		}
-	}
-
-	return $result[0]->attributes()->token;
-}
-
 function deletepage( $pageid, $deleteToken ) {
 	global $settings;
-
-	$url = $settings['publicWiki'] . "/api.php?action=delete&format=xml";
-	$params = "action=delete&pageid=$pageid&token=$deleteToken&reason=Outdated";
-	httpRequest($url, $params);
-	// Nothing to do with response currently
-	// $data = httpRequest($url, $params);
-	// $xml = simplexml_load_string($data);
+	deleteId($settings['publicWiki'], $pageid, $deleteToken);
 }
 
 function copypage( $pageName, $editToken, $recursivelyCalled = true ) {
@@ -138,17 +52,12 @@ function copypage( $pageName, $editToken, $recursivelyCalled = true ) {
 	}
 
 	// now copy normal page
-	$content = urlencode( $content );
-	$url = $settings['publicWiki'] . "/api.php?format=xml&action=edit&title=$pageName";
-	$data = httpRequest($url, $params = "format=xml&action=edit&title=$pageName&text=$content&token=$editToken");
+	$data = exportPage($settings['publicWiki'], $pageName, $content, $editToken);
 	if ( $data == null ) {
 		// write to file that copy failed
 		echo "logging page name in failed_pages.txt\n";
 		file_put_contents( 'failed_pages.txt' , $pageName . "\n", FILE_APPEND );
 	}
-
-	$xml = simplexml_load_string($data);
-	errorHandler( $xml );
 
 	// Now import images linked on the page
 	echo "Finding file links in $pageName ...\n";
@@ -186,34 +95,3 @@ function copypage( $pageName, $editToken, $recursivelyCalled = true ) {
 	}
 }
 
-function errorHandler( $xml ){
-	if( property_exists( $xml, 'error' ) ) {
-		$errors = is_array( $xml->error )? $xml->error : array( $xml->error );
-		foreach( $errors as $error ) {
-			echo "Error code: " . $error['code'] . " " . $error['info'] . "\n";
-		}
-	}
-}
-
-function download($url, $file_target) {
-	global $settings;
-	$fp = fopen ( $file_target, 'w+');//This is the file where we save the information
-	$ch = curl_init(str_replace(" ","%20",$url));//Here is the file we are downloading, replace spaces with %20
-	if( $settings['serverAuth'] ) {
-		curl_setopt($ch, CURLOPT_USERPWD, $settings['AuthUsername'] . ":" . $settings['AuthPassword']);
-	}
-	curl_setopt($ch, CURLOPT_URL, ($url));
-//	curl_setopt( $ch, CURLOPT_ENCODING, "UTF-8" );
-//	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_COOKIEFILE, $settings['cookiefile']);
-	curl_setopt($ch, CURLOPT_COOKIEJAR, $settings['cookiefile']);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($ch, CURLOPT_TIMEOUT, 50);
-	curl_setopt($ch, CURLOPT_FILE, $fp); // write curl response to file
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-	$xml = curl_exec($ch); // get curl response
-	curl_close($ch);
-	fclose($fp);
-	return $xml;
-}
--- wikiImporter.php
+++ wikiImporter.php
@@ -9,25 +9,16 @@ error_reporting( E_STRICT );
 
 include( 'settings.php' );
 $settings['cookiefile'] = "cookies.tmp";
+
+include 'MediaWiki_Api\MediaWiki_Api_functions.php';
 include( 'helperfunctions.php' );
 
+$publicApi = new MediaWikiApi($settings['publicWiki']);
 echo "Logging in to public wiki\n";
-
-try {
-	global $settings;
-	$token = login($settings['publicWiki'],$settings['publicWikiUser'], $settings['publicWikiPassword']);
-	login($settings['publicWiki'],$settings['publicWikiUser'], $settings['publicWikiPassword'], $token);
-	echo ("SUCCESS\n");
-} catch (Exception $e) {
-	die("FAILED: " . $e->getMessage() . "\n");
-}
-//get token first
-$url = $settings['publicWiki'] . "/api.php?format=xml&action=query&titles=Main_Page&prop=info|revisions&intoken=edit";
-$data = httpRequest($url, $params = '');
-$xml = simplexml_load_string($data);
-$editToken = urlencode( (string)$xml->query->pages->page['edittoken'] );
+$publicApi->login($settings['publicWikiUser'], $settings['publicWikiPassword']);
 
 echo "Starting to delete pages one by one in public wiki... \n";
+
 //get pagenames that shouldn't be deleted
 $doNotDeletePages = file( $settings['doNotDeletePages'], FILE_IGNORE_NEW_LINES );
 
@@ -38,33 +29,25 @@ for( $i=0; $i<15; $i++ ) {
 		continue;
 	}
 
-	$url = $settings['publicWiki'] . "/api.php?action=query&list=allpages&format=xml&apnamespace=$i&aplimit=10000"; // Hope this limit is enough large that we don't have the trouble to do this again and again using 'continue'
-	$data = httpRequest($url, $params = '');
-	$xml = simplexml_load_string($data);
-	$expr = "/api/query/allpages/p";
-	$result = $xml->xpath($expr);
+	$result = $publicApi->listPageInNamespace($i);
 	foreach( $result as $page ) {
 		if( !in_array( (string)$page['title'], $doNotDeletePages ) ) {
 			echo "Deleting page ". (string)$page['title'] . "\n";
-			deletepage( (string)$page['pageid'], $editToken );
+			$publicApi->deleteById((string)$page['pageid']);
 		} else {
 			echo "Skipping page ". (string)$page['title'] . "\n";
 		}
 	}
 }
+
 //all deletion done now :)
 
 //copy pages
-//now login to other wiki
+
+$privateApi = new MediaWikiApi($settings['privateWiki']);
+
 echo "Logging into private wiki now\n";
-try {
-	global $settings;
-	$token = login($settings['privateWiki'],$settings['privateWikiUser'], $settings['privateWikiPassword']);
-	login($settings['privateWiki'],$settings['privateWikiUser'], $settings['privateWikiPassword'], $token);
-	echo ("SUCCESS\n");
-} catch (Exception $e) {
-	die("FAILED: " . $e->getMessage() . "\n");
-}
+$privateApi->login($settings['privateWikiUser'], $settings['privateWikiPassword']);
 
 echo "Starting to import pages, categories and files...\n";
 
